a328cca3297ea4006d3c778c9137db6946a018ac
anelbolatova <anellbolatova2003@gmail.com>
2024-01-31 21:44:16 +0600

    Solved all exercises from home till casting

--- main.py
+++ main.py
@@ -1,2 +1,118 @@
-#Python HOME
-print("Hello World")
+#Python Syntax
+
+#1
+#print("Hello World")
+
+#2
+# if 5 > 2:
+#     print("YES")
+
+#Python Comments
+
+#1
+#This is a comment
+
+#2
+'''
+This is a comment
+written in 
+more than just one line
+'''
+
+#Python Variables
+#1
+#carname = "Volvo"
+#2
+#x = 50
+#3
+'''
+x = 5
+y = 10
+print(x + y)
+'''
+#4
+'''
+x = 5
+y = 10
+z = x + y
+print(z)
+'''
+#5
+#x,y,z = "Orange", "Banana", "Cherry"
+#6
+#x = y = z = "Orange"
+#7
+'''
+def myFunc():
+    global x
+    x = "fantastic"
+'''
+
+#Python Data Types
+#1
+'''
+x = 5
+print(type(x))
+
+print("int")
+'''
+#2
+'''
+x = "Hello World"
+print(type(x))
+
+print("str")
+'''
+#3
+'''
+x = 20.5
+print(type(x))
+
+print("float")
+'''
+#4
+'''
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+print("list")
+'''
+#5
+'''
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+print("tuple")
+'''
+#6
+'''
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+print("dict")
+'''
+#7
+'''
+x = True
+print(type(x))
+
+print("bool")
+'''
+
+
+#Python Numbers
+#1
+'''
+x = 5
+x = float(x)
+'''
+#2
+'''
+x = 5.5
+x = int(x)
+'''
+#3
+'''
+x = 5
+x = complex(x)
+'''
